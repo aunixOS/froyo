14ac2059608deeddb2c55be301bf76b058a9d2dc
Mikael Varshovsky <aunix.os@gmail.com>
2021-11-23 14:34:57 -0600

    almost complete redesign of froyo
    multi package is now supported, as well as some basic syntax

--- froyo.py
+++ froyo.py
@@ -2,13 +2,32 @@
 import sys
 import os # this is for pulling the files from the aur
 
-#get the package to install
+#make syntax for the command
+args = sys.argv
+lenargs = len(args)
 
+#the -S is going to be second
+instructor = args[1]
 
-try:
-    print("https://aur.archlinux.org/" + sys.argv[1] + ".git")
-    os.system("git clone https://aur.archlinux.org/" + sys.argv[1] + ".git")
-    os.chdir(sys.argv[1])
+if (instructor == "-S"):
+    print("sync requested")
+#do some math to figure out the packages
+packages = []
+packagecount = 0
+
+x = 1
+while True:
+    try:
+        print(args[x])
+        x += 1
+        packages.append(args[x])
+        packagecount += 1
+        print(packages)
+    except:
+        if (IndexError):
+            break
+
+for package in packages:
+    os.system("git clone https://aur.archlinux.org/" + package + ".git")
+    os.chdir(package)
     os.system("makepkg -s -si")
-except:
-    pass
