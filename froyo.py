c3b4617dabf7c3acab7288ec24f4a8c6ac136952
Mikael Varshovsky <aunix.os@gmail.com>
2021-11-26 10:35:03 -0600

    added the ability to remove packages

--- froyo.py
+++ froyo.py
@@ -11,23 +11,36 @@ instructor = args[1]
 
 if (instructor == "-S"):
     print("sync requested")
-#do some math to figure out the packages
-packages = []
-packagecount = 0
+    #do some math to figure out the packages
+    packages = []
+    packagecount = 0
+    x = 1
+    while True:
+        try:
+            print(args[x])
+            x += 1
+            packages.append(args[x])
+            packagecount += 1
+            print(packages)
+        except:
+            if (IndexError):
+                break
+        for package in packages:
+            os.system("git clone https://aur.archlinux.org/" + package + ".git")
+            os.chdir(package)
+            os.system("makepkg -s -si")
+if (instructor == "-R"):
+    packages = []
+    packagecount = 0
+    x = 1
+    while True:
+        try:
+            x += 1
+            packages.append(args[x])
+            packagecount += 1
+        except:
+            if (IndexError):
+                break
 
-x = 1
-while True:
-    try:
-        print(args[x])
-        x += 1
-        packages.append(args[x])
-        packagecount += 1
-        print(packages)
-    except:
-        if (IndexError):
-            break
-
-for package in packages:
-    os.system("git clone https://aur.archlinux.org/" + package + ".git")
-    os.chdir(package)
-    os.system("makepkg -s -si")
+        for package in packages:
+            os.system("sudo rm -rf /bin/" + package)
